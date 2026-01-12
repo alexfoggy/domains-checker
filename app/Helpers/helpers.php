f782e532c46679446103113ffdfb349539c30e2e
Igor Ejov <46809948+alexfoggy@users.noreply.github.com>
2026-01-12 17:24:54 +0200

    Update helpers.php

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -125,6 +125,7 @@ class Helper
         $json = json_encode($xml);
         $array = json_decode($json, TRUE);
 
+        dd($array);
         if ($array['CommandResponse']['DomainCheckResult']) {
             foreach ($array['CommandResponse']['DomainCheckResult'] as $row) {
                 if ($row['@attributes']['Available'] == "true") {
