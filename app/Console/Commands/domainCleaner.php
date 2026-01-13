7fd9035f928fce0b8eb1b5c945bc0572464d2601
Igor Ejov <46809948+alexfoggy@users.noreply.github.com>
2026-01-13 19:06:10 +0200

    Create domainCleaner.php

---
+++ app/Console/Commands/domainCleaner.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\DomainToCheck;
+use Illuminate\Console\Command;
+
+class domainCleaner extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'domains:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command start removing double domains';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $domains = DomainToCheck::all();
+        $domains->groupBy('domain');
+        foreach ($domains as $domain_group) {
+            if ($domain_group->count() > 1) {
+                dd($domain_group);
+            }
+        }
+    }
+}
